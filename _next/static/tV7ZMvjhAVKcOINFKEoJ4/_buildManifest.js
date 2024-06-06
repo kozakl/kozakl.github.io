c691132e5c12d702bdb62ee1d8a1878d9a625989
Luke Kozak <kozakluke@gmail.com>
2024-06-06 12:47:12 +0200

    Auto Deploy

--- _next/static/tV7ZMvjhAVKcOINFKEoJ4/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/240-64770599aae3c7b7.js","static/css/6445fda4f7eeffef.css","static/chunks/pages/index-36c1dffbf3f9921b.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
