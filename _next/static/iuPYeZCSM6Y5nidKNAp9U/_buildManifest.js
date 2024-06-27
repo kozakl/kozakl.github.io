228bc331428bc34c953dfbe293bd5f39f67ecf64
Luke Kozak <kozakluke@gmail.com>
2024-06-27 09:14:16 +0200

    Auto Deploy

--- _next/static/iuPYeZCSM6Y5nidKNAp9U/_buildManifest.js
+++
@@ -1,3 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/814-5934be19cff1038d.js","static/css/a529ace462bcc7f7.css","static/chunks/pages/index-9b49734e2dbf4a63.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();Cache-Control: max-age=2592000
-Cache-Control: max-age=2592000
-Cache-Control: max-age=2592000
